56245bc50b3e48dcfad40b42d921053e441b2caa
Ferdinando Simonetti <ferdinando.simonetti@outlook.com>
2020-12-04 16:21:41 +0100

    usato if __name__ anche nel programma principale

--- tictactoe.py
+++ tictactoe.py
@@ -1,33 +1,33 @@
 from TicTacToe import initialize,clearscreen,redraw,fulltable,playablemove,playermove,win
-
 # empty table
 gametable = []
 # obviously illegal move
 move      = "#"
 
-# Start game, the table is empty
-gametable = initialize(gametable)
-redraw(gametable)
-
-print("X plays first, then O")
-player = 'X'
-
-# while loop continues until X wins, O wins or there are no more playable moves
-while not win('X',gametable) and not win('O',gametable) and not fulltable(gametable):
-  print(f"It's {player} turn!")
-  # internal while loop continues until the player provides a 'legal' move
-  while True:
-    status, move = playermove(move,gametable)
-    if status:
-      break
-  # list is updated
-  gametable[int(move)] = player
-  # and game table is redrawn
+if __name__ == "__main__":
+  # Start game, the table is empty
+  gametable = initialize(gametable)
   redraw(gametable)
-  # players should alternate
-  if player == 'X':
-    player = 'O'
-  else:
-    player = 'X'
 
-print('End game')
+  print("X plays first, then O")
+  player = 'X'
+
+  # while loop continues until X wins, O wins or there are no more playable moves
+  while not win('X',gametable) and not win('O',gametable) and not fulltable(gametable):
+    print(f"It's {player} turn!")
+    # internal while loop continues until the player provides a 'legal' move
+    while True:
+      status, move = playermove(move,gametable)
+      if status:
+        break
+    # list is updated
+    gametable[int(move)] = player
+    # and game table is redrawn
+    redraw(gametable)
+    # players should alternate
+    if player == 'X':
+      player = 'O'
+    else:
+      player = 'X'
+    
+    print('End game')
